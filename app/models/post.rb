778f55ca5f0d9926ff77815544f455ff8747b799
kierra <51520037+kierrakay@users.noreply.github.com>
2020-01-20 20:13:31 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :is_clickbait
+    validates :content, length: {minimum: 100}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+
+
+    def is_clickbait
+        included = false
+        @@phrases.each { |phrase| included = true if self.title && self.title.downcase =~ phrase }
+        if not included
+            errors.add(:title, "You need a more exciting title")
+        end
+    end
+
+    @@phrases = [/won't believe/, /secret/, /top \d/, /guess/]
 end
+# =~ is known as the "match operator" and can be used to match a string against a regular expression
+#regex
