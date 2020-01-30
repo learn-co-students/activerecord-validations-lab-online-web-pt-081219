d4e941bbb4520bc643b38e2dc2f18ef22b7fa7df
github username <github email address>
2020-01-30 10:05:22 -0500

    Done.

---
+++ app/validators/clickbait_title_validator.rb
@@ -0,0 +1,8 @@
+Learn more or give us feedback
+class ClickbaitTitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\b(Won't Believe|Secret|Top \d|Guess)\b/
+      record.errors[attribute] << "Title is not clickbaity enough"
+    end
+  end
+end
